LineageOS/android_vendor_qcom_opensource_power

power: Follow symlinks when writing to dt2w toggle path
* Some devices tend to symlink touchpanel related toggles
  to common path due to having to deal with different panels.

Change-Id: I429e4c5acfbcd3ff26a019f919fbe880692160bd

Power.cpp
@@ -71,7 +71,7 @@ Return<void> Power::setFeature(Feature feature, bool activate) {
     switch (feature) {
 #ifdef TAP_TO_WAKE_NODE
         case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
-            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE);
+            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE, true);
             break;
 #endif
         default:

Inkypen79/android_device_xiaomi_msm8996-common

power-libperfmgr/Power.cpp
@@ -231,7 +231,7 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
 Return<void> Power::setFeature(Feature feature, bool activate)  {
     if (feature == Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE) {
         ALOGV("%s: %s double tap to wake", __func__, activate ? "enabling" : "disabling");
-        ::android::base::WriteStringToFile(activate ? "1" : "0", DT2W_PATH);
+        ::android::base::WriteStringToFile(activate ? "1" : "0", DT2W_PATH, true);
     }
     return Void();
 }