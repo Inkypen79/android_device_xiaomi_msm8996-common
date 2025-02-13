28400997219a9eec6a85d143bb46c64e59d525fd
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:40:58 +0800

    msm8996-common: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.4) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- msm8996.mk
+++ msm8996.mk
@@ -392,6 +392,9 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
