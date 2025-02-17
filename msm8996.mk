c779045740c3e17f9e362e827da3a32bd0cea057
Flicker372 <flicker372@outlook.com>
2025-02-17 15:42:23 +0800

    msm8996-common: Import task_profiles_30.json from android-15.0.0_r17
    Change-Id: I456ad023840ad81b9ff0b4a170915b6d6fc6c0c7

--- msm8996.mk
+++ msm8996.mk
@@ -321,7 +321,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
