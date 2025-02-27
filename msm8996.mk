c35e3550449c4d29675c3c88a9644604e7e9ba7d
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    msm8996-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- msm8996.mk
+++ msm8996.mk
@@ -252,6 +252,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
@@ -394,9 +397,6 @@ PRODUCT_BOOT_JARS += \
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
-
-# UFFD GC
-OVERRIDE_ENABLE_UFFD_GC := false
 
 # USB
 PRODUCT_PACKAGES += \
