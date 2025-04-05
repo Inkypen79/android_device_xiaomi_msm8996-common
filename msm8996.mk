de7db288b2ca4b106d4a2494f3a45f0f00462cac
Alcatraz323 <alcatraz32323@gmail.com>
2025-04-05 22:10:55 +0800

    msm8996-common: Import cgroup v2 config from android-15.0.0_r14
    Change-Id: I970e1ac58373ed59a5ede4648f216de035e188c3

--- msm8996.mk
+++ msm8996.mk
@@ -317,7 +317,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # Protobuf
